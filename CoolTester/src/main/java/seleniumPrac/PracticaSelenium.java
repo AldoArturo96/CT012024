1354c9f84bc95a4ef4ecdba7eb1b27dc3072f7fc
Aldo A Francisco B <Aldo A Francisco B@HDFFRANCISCOAA>
2024-01-20 13:58:24 -0600

    Primer commit

---
+++ CoolTester/src/main/java/seleniumPrac/PracticaSelenium.java
@@ -0,0 +1,16 @@
+package seleniumPrac;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class PracticaSelenium {
+
+	public static void main(String[] args) {
+		
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://uatportalpagos.claropay.com/home/default");
+
+	}
+
+	
+}
