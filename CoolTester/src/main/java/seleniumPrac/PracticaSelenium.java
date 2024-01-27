fdd8dca5025ee8eda5dbdb158ffea0dc4b45d891
Aldo A Francisco B <Aldo A Francisco B@HDFFRANCISCOAA>
2024-01-27 11:55:29 -0600

    Tercer commit

--- CoolTester/src/main/java/seleniumPrac/PracticaSelenium.java
+++ CoolTester/src/main/java/seleniumPrac/PracticaSelenium.java
@@ -8,7 +8,7 @@ public class PracticaSelenium {
 	public static void main(String[] args) {
 		
 		WebDriver driver = new ChromeDriver();
-		driver.get("https://www.google.com.mx/?hl=es");
+		driver.get("https://www.google.com.mx");
 
 	}
 
